cf89959fc6915ef61c4a603c3a424f5cf6da425d
jesseneon <jessejoseph64@gmail.com>
2023-04-25 13:42:14 +0100

    fix: add react-loader-spinner to package.json

---
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./DashboardLayout.module.scss";
+
+const DashboardLayout: React.FC = () => {
+  return <div>DashboardLayout</div>;
+};
+
+export default DashboardLayout;
