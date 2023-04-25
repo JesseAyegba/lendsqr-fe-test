f8920ac50cedd70c1654b2d1726a51f128d4fc95
jesseneon <jessejoseph64@gmail.com>
2023-04-25 13:46:47 +0100

    fix: add default export to dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Head>
+        <title>Lendsqr | Dashboard</title>
+        <meta name="Dashboard | Home" content="The Lendsqr Dashboard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div></div>
+    </>
+  );
+};
+
+export default Dashboard;
